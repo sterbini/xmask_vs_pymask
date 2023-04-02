769791c780e312512f73648095d30f83784daf20
Guido Sterbini <guido.sterbini@cern.ch>
2023-04-02 12:05:38 +0200

    status

--- 001_run.sh
+++ 001_run.sh
@@ -1,24 +1,26 @@
-cd 010_reference_lines/hl_lhc_collisions_offbb_offerrors
+cd 010_reference_lines
+
+cp hl_lhc_collisions_offbb_offerrors_b1/clean_it.sh hl_lhc_collisions_offbb_onerrors_b1/
+cp hl_lhc_collisions_offbb_offerrors_b1/clean_it.sh hl_lhc_collisions_onbb_offerrors_b1/
+cp hl_lhc_collisions_offbb_offerrors_b1/clean_it.sh hl_lhc_collisions_onbb_onerrors_b1/
+
+cp hl_lhc_collisions_offbb_offerrors_b1/*.py hl_lhc_collisions_offbb_onerrors_b1/
+cp hl_lhc_collisions_offbb_offerrors_b1/*.py hl_lhc_collisions_onbb_offerrors_b1/
+cp hl_lhc_collisions_offbb_offerrors_b1/*.py hl_lhc_collisions_onbb_onerrors_b1/
+
+cd hl_lhc_collisions_offbb_offerrors_b1
 python 000_pymask.py > output.log 2> error.log &
 cd ../
 
-cp hl_lhc_collisions_offbb_offerrors/clean_it.sh hl_lhc_collisions_offbb_onerrors/
-cp hl_lhc_collisions_offbb_offerrors/clean_it.sh hl_lhc_collisions_onbb_offerrors/
-cp hl_lhc_collisions_offbb_offerrors/clean_it.sh hl_lhc_collisions_onbb_onerrors/
-
-cp hl_lhc_collisions_offbb_offerrors/*.py hl_lhc_collisions_offbb_onerrors/
-cp hl_lhc_collisions_offbb_offerrors/*.py hl_lhc_collisions_onbb_offerrors/
-cp hl_lhc_collisions_offbb_offerrors/*.py hl_lhc_collisions_onbb_onerrors/
-
-cd hl_lhc_collisions_offbb_onerrors
+cd hl_lhc_collisions_offbb_onerrors_b1
 python 000_pymask.py > output.log 2> error.log &
 cd ../
 
-cd hl_lhc_collisions_onbb_offerrors
+cd hl_lhc_collisions_onbb_offerrors_b1
 python 000_pymask.py > output.log 2> error.log &
 cd ../
 
-cd hl_lhc_collisions_onbb_onerrors
+cd hl_lhc_collisions_onbb_onerrors_b1
 python 000_pymask.py > output.log 2> error.log &
 cd ../../
 
